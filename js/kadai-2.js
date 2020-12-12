1bca5c18d1083a690989c4a074b97bcb5371e573
newyee <takatelin01@icloud.com>
2020-12-12 13:26:25 +0900

    エラー構文の修正

--- js/kadai-2.js
+++ js/kadai-2.js
@@ -8,25 +8,19 @@ window.addEventListener('DOMContentLoaded', function(e){
     let fizz_num = document.getElementById("fizz_num").value;
     let buzz_num = document.getElementById("buzz_num").value;
     let err_msg = '';
-    if(fizz_num === '' || buzz_num === ''){
-      err_msg = '1以上99以下の整数を入力してください'
-    }else{
-      fizz_num = Number(fizz_num);
-      buzz_num = Number(buzz_num);
-      if(fizz_num || buzz_num){
-        if(!Number.isInteger(fizz_num) || !Number.isInteger(buzz_num) ){
-          err_msg = '1以上99以下の整数を入力してください';
-        }
-      }else{
-        err_msg = '1以上99以下の整数を入力してください';
-      }
+    fizz_num = Number(fizz_num);
+    buzz_num = Number(buzz_num);
+
+    // console.log('fizz_num',fizz_num)
+    if(!fizz_num){
+      console.log('aaaa')
     }
-    if(err_msg){
+    if(!fizz_num || !buzz_num || fizz_num > 99 || buzz_num > 99){
+      err_msg = '1以上99以下の整数を入力してください'
       let tag_p = document.getElementById("tag_p");
       let display_err_msg = document.createTextNode(err_msg);
       tag_p.appendChild(display_err_msg);
-    }
-    if(!err_msg){
+    }else{
       let display_fizz_value = '';
       let display_buzz_value = '';
       let display_fizz_buzz_value = '';
